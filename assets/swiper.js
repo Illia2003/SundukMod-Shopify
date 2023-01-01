eb3702c07a6bc61cabade769a8345be0984a5e8a
Illia2003 <ilia.gumeniuk2018@gmail.com>
2023-01-01 20:52:01 +0200

    Added scrollbar to wrapper

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,9 +1,18 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+import Swiper, {
+  Scrollbar,
+} from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
 if (document.querySelector(".collection__swiper")) {
   const collectionSwiper = new Swiper(".collection__swiper", {
     spaceBetween: 40,
     loop: false,
     slidesPerView: "auto",
+
+    modules: [Scrollbar],
+
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+    },
   });
 }
