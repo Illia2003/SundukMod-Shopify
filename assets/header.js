389df46b9743f42446b7575328c06874067531de
Illia2003 <ilia.gumeniuk2018@gmail.com>
2023-02-27 15:01:44 +0200

    Added smooth scroller

--- assets/header.js
+++ assets/header.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  var headerTl = gsap.timeline();
+var headerTl = gsap.timeline();
 
-  headerTl.fromTo(
-    ".header",
-    {
-      y: "-100%",
-      transformOrigin: "50% 50%",
-    },
-    {
-      y: 0,
-      ease: "slow(0.1, 0.4, false)",
-      duration: 0.5,
-    }
-  );
-});
+headerTl.fromTo(
+  ".header",
+  {
+    y: "-100%",
+    transformOrigin: "50% 50%",
+  },
+  {
+    y: 0,
+    ease: "slow(0.1, 0.4, false)",
+    duration: 0.5,
+  }
+);
+
+globalTl.add(headerTl, "headerTl");
--- assets/hero.js
+++ assets/hero.js
@@ -1,65 +1,44 @@
-document.addEventListener("DOMContentLoaded", () => {
-  var heroTl = gsap.timeline();
+// var heroTl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".hero",
+//     start: "top top",
+//     markers: true,
+//   },
+// });
 
-  heroTl
-    .fromTo(
-      ".hero .btn__svg--first svg path",
-      {
-        scale: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-      },
-      {
-        scale: 1,
-        duration: 1.5,
-        ease: "slow(0.1, 0.4, false)",
-      }
-    )
-    .fromTo(
-      ".hero .btn__svg--first svg path",
-      {
-        opacity: 1,
-        transformOrigin: "50% 50%",
-      },
-      {
-        opacity: 0,
-        duration: 0.7,
-        ease: "slow(0.1, 0.4, false)",
-      },
-      "-=0.6"
-    );
+heroTl = gsap.timeline();
 
-  heroTl
-    .fromTo(
-      ".hero .btn__svg--second svg path",
-      {
-        scale: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-      },
-      {
-        scale: 1,
-        duration: 1.5,
-        ease: "slow(0.1, 0.4, false)",
-      },
-      "-=1"
-    )
-    .fromTo(
-      ".hero .btn__svg--second svg path",
-      {
-        opacity: 1,
-        transformOrigin: "50% 50%",
-      },
-      {
-        opacity: 0,
-        duration: 0.7,
-        ease: "slow(0.1, 0.4, false)",
-      },
-      "-=0.6"
-    );
+heroTl
+  .fromTo(
+    ".hero .btn__svg--first svg path",
+    {
+      scale: 0,
+      opacity: 1,
+      transformOrigin: "50% 50%",
+    },
+    {
+      scale: 1,
+      duration: 1.5,
+      ease: "slow(0.1, 0.4, false)",
+    }
+  )
+  .fromTo(
+    ".hero .btn__svg--first svg path",
+    {
+      opacity: 1,
+      transformOrigin: "50% 50%",
+    },
+    {
+      opacity: 0,
+      duration: 0.7,
+      ease: "slow(0.1, 0.4, false)",
+    },
+    "-=0.6"
+  );
 
-  heroTl.fromTo(
-    ".hero .btn__content svg path",
+heroTl
+  .fromTo(
+    ".hero .btn__svg--second svg path",
     {
       scale: 0,
       opacity: 1,
@@ -71,40 +50,69 @@ document.addEventListener("DOMContentLoaded", () => {
       ease: "slow(0.1, 0.4, false)",
     },
     "-=1"
-  );
-
-  heroTl.fromTo(
-    ".hero .btn__content .animation .btn__text",
+  )
+  .fromTo(
+    ".hero .btn__svg--second svg path",
     {
-      y: "100%",
+      opacity: 1,
+      transformOrigin: "50% 50%",
     },
     {
-      y: 0,
-      duration: 0.5,
+      opacity: 0,
+      duration: 0.7,
       ease: "slow(0.1, 0.4, false)",
-    }
+    },
+    "-=0.6"
   );
 
-  heroTl.to(".section__title .animation", {
-    y: 0,
-    duration: 0.75,
-    delay: (index) => {
-      return 0.15 * index;
-    },
+heroTl.fromTo(
+  ".hero .btn__content svg path",
+  {
+    scale: 0,
+    opacity: 1,
+    transformOrigin: "50% 50%",
+  },
+  {
+    scale: 1,
+    duration: 1.5,
     ease: "slow(0.1, 0.4, false)",
-  });
+  },
+  "-=1"
+);
 
-  heroTl.fromTo(
-    ".hero .hero__bottom img",
-    {
-      y: "100%",
-      scale: 0,
-    },
-    {
-      y: 0,
-      scale: 1,
-      duration: 1,
-      ease: "slow(0.1, 0.4, false)",
-    }
-  );
+heroTl.fromTo(
+  ".hero .btn__content .animation .btn__text",
+  {
+    y: "100%",
+  },
+  {
+    y: 0,
+    duration: 0.5,
+    ease: "slow(0.1, 0.4, false)",
+  }
+);
+
+heroTl.to(".section__title .animation", {
+  y: 0,
+  duration: 0.75,
+  delay: (index) => {
+    return 0.15 * index;
+  },
+  ease: "slow(0.1, 0.4, false)",
 });
+
+heroTl.fromTo(
+  ".hero .hero__bottom img",
+  {
+    y: "100%",
+    scale: 0,
+  },
+  {
+    y: 0,
+    scale: 1,
+    duration: 1,
+    ease: "slow(0.1, 0.4, false)",
+  }
+);
+
+globalTl.add(heroTl, "heroTl");
