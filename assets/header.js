f71729ba9346a4241c9cfdacb458c26aa54ce8f6
Illia2003 <ilia.gumeniuk2018@gmail.com>
2023-08-09 13:48:02 +0300

    Home page. Updates

--- assets/header.js
+++ assets/header.js
@@ -1,20 +1,4 @@
-var headerTl = gsap.timeline();
 let lastScrollPosition = window.scrollY;
-
-headerTl.fromTo(
-  ".header",
-  {
-    y: -1 * document.querySelector(".header").offsetHeight,
-    transformOrigin: "50% 50%",
-  },
-  {
-    y: 0,
-    ease: "slow(0.1, 0.4, false)",
-    duration: 0.5,
-  }
-);
-
-globalTl.add(headerTl, "headerTl");
 
 window.onscroll = () => {
   var header = document.querySelector(".header");
